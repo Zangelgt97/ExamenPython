95bc6bcd6abb66254b9172024e2a170299245a58
[Zangelgt97] <[zangelgt97@gmail.com]>
2022-09-21 10:08:14 -0500

    scm

---
+++ Ejercicio1-3/andrea.py
@@ -0,0 +1,19 @@
+
+#este es el programa de andrea
+x=4+5;
+
+edad_de_amdrea=input("andrea dime tu edad ")
+print(edad_de_amdrea)
+apellido=str(input("andrea dime tus apellidos"))
+print("tu apellido es ",str(apellido))
+añosde_de_naci=2022-int(edad_de_amdrea)+x
+print("andrea tu naciste en ",añosde_de_naci)
+if int(edad_de_amdrea)<18:
+    print("andrea es menor de edad")
+else:
+    print("andrea es mayor de edad")
+print("andrea esta castigada y tiene que escribir 1000 veces su nombre ")
+i=1;
+for i in range(1000):
+    i+1
+    print(apellido)
